55c395e1484a3eb5f51489a852ae0112cfa327ea
Enrique Alcántara <enrique_eduaa@ciencias.unam.mx>
2022-03-23 21:37:02 -0600

    Agregado el sprite del enemigo

---
+++ Assets/Scripts/Enemigo.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemigo : MonoBehaviour
+{
+
+    // Vida del enemigo
+    [SerializeField]
+    private int vida;
+
+    // Velocidad del enemigo
+    [SerializeField]
+    private float velocidad;
+
+
+    
+}
